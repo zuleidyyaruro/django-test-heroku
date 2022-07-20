def0242efcfae143dac35f23ff8aedfbd7459066
Zuleidy Yaruro <49843884+zuleidyyaruro@users.noreply.github.com>
2022-07-20 13:34:38 -0500

    first commit

---
+++ pages/tests.py
@@ -0,0 +1,15 @@
+from django.test import SimpleTestCase
+
+# Create your tests here.
+
+
+class HomePageView(SimpleTestCase):
+    def test_url_exists_at_correct_location(self):
+        response = self.client.get("/")
+        self.assertEqual(response.status_code, 200)
+
+
+class AboutPageView(SimpleTestCase):
+    def test_url_exists_at_correct_location(self):
+        response = self.client.get("/about/")
+        self.assertEqual(response.status_code, 200)
